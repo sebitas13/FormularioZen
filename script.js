64a72ffbee45e17c113644e4c2d3e03997b7a066
sebas <jesus.huamanculi1@unmsm.edu.pe>
2024-01-09 19:30:19 -0500

    flex-colum

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const contenedor = document.querySelector('section');
 const login_box = document.querySelector('.signUpBtn');
 const register_box = document.querySelector('.signInBtn');
+const body_box = document.querySelector('body');
 
 login_box.addEventListener('click',()=>{
     contenedor.classList.toggle('active');
+    body_box.classList.toggle('active');
 })
 
 
 register_box.addEventListener('click',()=>{
     contenedor.classList.toggle('active');
+    body_box.classList.toggle('active');
 })
 
 
